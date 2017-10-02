01bd74aa12f854351192f780b9d456d7212b39d7
adam.gajzlerowicz <nelf86@gmail.com>
2017-10-02 21:11:11 +0200

    trying to move to progamm

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,20 +1,23 @@
 module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+--import Html.Events exposing ( onClick )
 
 -- component import example
-import Components.Hello exposing ( hello )
+--import Components.Hello exposing ( hello )
 
 
 -- APP
-main : Program Never Int Msg
+main : Program Never Model Msg
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 
 -- MODEL
-type alias Model = Int
+type alias Model =
+    {
+        lunchAt: String
+    }
 
 model : number
 model = 0
@@ -25,39 +28,25 @@ type Msg = NoOp | Increment
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    model
+--  case msg of
+--    NoOp -> model
+--    Increment -> model + 1
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    div [id "page-wrapper"] [
+        div [
+            class "container"
+        ][
+            div [class "sto-trzy"]
+            []
+            , div [class "sto-piec"]
+            []
         ]
-      ]
+        , div [class "inner-container"]
+            [
+             text "dupa"
+            ]
     ]
-  ]
-
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
